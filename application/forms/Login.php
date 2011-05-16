7b94248935b51dc9f772e5d85f413a884f297aa4
Andy Tsen <your_email@youremail.com>
2011-05-15 21:39:26 -0700

    Uploading initial source

---
+++ application/forms/Login.php
@@ -0,0 +1,37 @@
+<?php
+
+class Application_Form_Login extends Zend_Form
+{
+
+    public function init()
+    {
+	
+    }
+    public function __construct($options = null){
+    	parent::__construct($options);
+    	$this->setName('login');
+		$this->setAction('/login/login');
+		$this->setMethod('post');
+		$email = new Zend_Form_Element_Text('email');
+		$email->setRequired(true)
+			  ->removeDecorator('label')
+			  ->removeDecorator('htmlTag');
+		
+		$password = new Zend_Form_Element_Password('password');
+		$password->setRequired(true)
+				 ->removeDecorator('label')
+				 ->removeDecorator('htmlTag');
+    	
+		$login = new Zend_Form_Element_Submit('login');
+		$login->removeDecorator('label')
+				 ->removeDecorator('htmlTag');
+		
+		$this->setDecorators(array(array('ViewScript',array('viewScript'=>'_form_login.phtml'))));
+		
+		$this->addElements(array($email,$password,$login));
+    	/* Form Elements & Other Definitions Here ... */
+    }
+
+
+}
+
