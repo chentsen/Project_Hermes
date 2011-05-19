0dfe51308265599a014ed41731bd1c17be295d84
Julian Tai <juliantai90@gmail.com>
2011-05-18 21:04:35 -0700

    Bootstrap DocType

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,12 @@
 use Wildkat\Application\Container\DoctrineContainer;
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected function _initDoctype()
+    {
+    $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('XHTML1_STRICT');
+    }
 	 public function _initDoctrineMongoContainer()
     {
         $container = new DoctrineContainer($this->getOption('doctrine'));
