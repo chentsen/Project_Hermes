7b7b92014afd8b292c3fcc934f1e8fff4ed857ed
Julian Tai <juliantai90@gmail.com>
2011-06-16 02:20:45 -0700

    js update

--- public/js/ptv1.js
+++ public/js/ptv1.js
@@ -1,1 +1,35 @@
-//nothing yet
+$(function()
+		{
+			$("input").blur(function()
+			{
+				var formElementId = $(this).get(0).name;
+				//find('label').attr('name'))
+				doValidation(formElementId);
+				console.log($(this).attr('name'));
+			});
+		});
+function doValidation(id)
+	{
+		var url='/index/validateform'
+		var data = {};
+		$("input").each(function()
+		{		
+			data[$(this).attr('name')] = $(this).val();
+		});
+		$.post(url,data,function(resp)
+		{
+			console.log(resp);
+			$("#"+id).parent().find('.errors').remove();
+			$("#"+id).parent().append(getErrorHtml(resp[id], id))
+		},'json');
+	});
+function getErrorsHtml (formerrors, id)
+	{
+		var o = '<ul id="errors-'+id+'" class="errors"';
+		for(errorKey in formErrors)
+			{
+				o += '<li>' + formErrors[errorKey] + '</li>';
+			}
+		o += '</ul>'
+			return o;
+	});
