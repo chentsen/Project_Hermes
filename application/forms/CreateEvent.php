ebfba88f64b67cbcc418bfbe9340fd244616ebdc
Julian Tai <juliantai90@gmail.com>
2011-08-29 17:37:01 -0700

    form improved

--- application/forms/CreateEvent.php
+++ application/forms/CreateEvent.php
@@ -12,11 +12,12 @@ class Application_Form_CreateEvent extends Zend_Form{
 		$shortDescription = new Zend_Form_Element_Text('createEvent_shortDescription');
 		$shortDescription->setRequired(true)
 						 ->addValidator('StringLength',array('max'=>40,'allowWhiteSpace'=>true));
+                                                 
 		//month stuff
-		$months = array('1'=>"Janurary",'Februrary'=>2,'March'=>3,'April'=>4,'May'=>5,'June'=>6,
+		/*$months = array('1'=>"Janurary",'Februrary'=>2,'March'=>3,'April'=>4,'May'=>5,'June'=>6,
 						'July'=>7,'August'=>8,'September'=>9,'October'=>10,'November'=>11,'December'=>12);
 		$month = new Zend_Form_Element_Select('createEvent_month');
-		$month->addMultiOptions($months);
+		$month->addMultiOptions($months);*/
 
 		$shortDescription = new Zend_Form_Element_Text('createEvent_shortDescription');
 		$shortDescription->setRequired(true)
@@ -25,12 +26,15 @@ class Application_Form_CreateEvent extends Zend_Form{
 		$location = new Zend_Form_Element_Text('createEvent_location');
 		$location->addValidator("StringLength", array('max'=>60,'allowWhiteSpace'=>true));
 		
-		$days = $this->generateDays();
+                
+                $date = new Zend_Form_Element_Text('createEvent_date');
+                
+		/*$days = $this->generateDays();
 		$day = new Zend_Form_Element_Select('createEvent_day');
 		$day->addMultiOptions($days);
 		
 		$year = new Zend_Form_Element_Select('createEvent_year');
-		$year->addMultiOption(2011,'2011');
+		$year->addMultiOption(2011,'2011');*/
 		
 		$private = new Zend_Form_Element_Select('createEvent_private');
 		$private->setMultiOptions(array("y"=>"yes","n"=>"no"));		
@@ -46,18 +50,18 @@ class Application_Form_CreateEvent extends Zend_Form{
 		
 		$moreOptions = new Zend_Form_Element_Button('createEvent_moreOptions');
 		
-		$this->addElements(array($day,$private,$location,$longDescription,$shortDescription,$month,$year,$submit,$moreOptions));
+		$this->addElements(array($date,$private,$location,$longDescription,$shortDescription,$submit,$moreOptions));
 		$this->setDecorators(array(array('ViewScript',array('viewScript'=>'_form_createEvent.phtml'))));
 		$this->setElementDecorators(array('ViewHelper','Errors'),null,false);
 	}
 	//wrong date. Javascript needs to be fixed on julian side to dynamically update with correct dates for placeholder purposes only
 	
-	private function generateDays(){
+	/*private function generateDays(){
 		$datesArray = array();
 		for($i = 1; $i<=30;$i++){
 			$datesArray["{$i}"]=$i;
 		}
 		return $datesArray;
-	}
+	}*/
 	
 }
