52e32f87537ec30e97bfc55da127bfabdf37a828
Julian Tai <juliantai90@gmail.com>
2011-09-10 17:15:33 -0700

    edit account

--- application/forms/EditAccount.php
+++ application/forms/EditAccount.php
@@ -14,12 +14,57 @@ class Application_Form_EditAccount extends Zend_Form
 		$this->setAction('/accountedit/index');
 		$this->setMethod('post');
                 
+		$firstName = new Zend_Form_Element_Text('firstName');
+		$firstName->setRequired(true)
+				  ->addValidator('StringLength',array('max'=>30,'allowWhiteSpace'=>true))
+				  ->addValidator('Regex',true, array(
+				  		'pattern' => '/^[a-zA-Z\-\' ]+$/',
+     					'messages' => array(
+          					'regexNotMatch' => 'Please enter a valid firstname.'
+    					 )
+  				  ))
+				  ->addValidator('NotEmpty', true, array(
+     					'messages' => array(
+          				'isEmpty' => 'A firstname is required.'
+    					 )
+  				  ));
 		
+		$lastName = new Zend_Form_Element_Text('lastName');
+		$lastName->setRequired(true)
+				  ->addValidator('StringLength',array('max'=>30,'allowWhiteSpace'=>true))
+				  ->addValidator('Regex',true, array(
+				 		 'pattern' => '/^[a-zA-Z\-\' ]+$/',
+     					'messages' => array(
+          				'regexNotMatch' => 'Please enter a valid lastname'
+    					 )
+  				  ))
+				  ->addValidator('NotEmpty', true, array(
+     					'messages' => array(
+          				'isEmpty' => 'A lastname is required.'
+    					 )
+  				  ));
+                $city = new Zend_Form_Element_Text('city');
+		$city->setRequired(true)
+				  ->addValidator('StringLength',array('max'=>40,'allowWhiteSpace'=>true))
+				  ->addValidator('Regex',true, array(
+				 	    'pattern' => '/^[a-zA-Z\-\' ]+$/',
+     					'messages' => array(
+          				'regexNotMatch' => 'Please enter a valid city name.'
+    					 )
+  				  ))
+				  ->addValidator('NotEmpty', true, array(
+     					'messages' => array(
+          				'isEmpty' => 'A city is required.'
+    					 )
+  				  ));
+		$submit = new Zend_Form_Element_Submit('submit');
+		$submit->removeDecorator('label')
+				->removeDecorator('htmlTag')
+				->removeDecorator('DtDdWrapper');
 		
 		$this->setDecorators(array(array('ViewScript',array('viewScript'=>'_form_accountEdit.phtml'))));
-		/*
-		$this->addElements(array($submit, $personality, $career, $major, $websites, $brands, $teams, $inorout, $cuisine, $news, $books, $bookswriters,
-                                            $musictype, $filmgenre, $films, $hangout, $artists, $friday, $sports, $videogames, $boardgames, $hobbies));*/
+		
+		$this->addElements(array($submit, $firstName, $city, $lastName));
 		$this->setElementDecorators(array('ViewHelper'),null,false);
                 
     }
