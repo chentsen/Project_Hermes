ee78b4fa01750caa9e666a2b084d251af9158ee9
Julian Tai <juliantai90@gmail.com>
2011-10-09 11:56:35 -0700

    tags js

--- public/js/tags.js
+++ public/js/tags.js
@@ -3,7 +3,19 @@
  * and open the template in the editor.
  */
 $(document).ready(function(){
+    $('.tags').click(function(){
+                $(this).toggleClass('tag_disable');
 
+            });
+            $('.close_tag').click(function(){
+                $(this).removeClass('tag_disable');
+
+            });
+
+
+            $('.close_tags a').click(function(){
+                $(this).addClass('tag_remove');
+            });
 
      
 });
