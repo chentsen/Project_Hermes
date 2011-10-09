5317593573510a56a922bc7c3691a8f9bc03e6e8
Andy Tsen <your_email@youremail.com>
2011-10-09 11:13:15 -0700

    jquery auto suggest implementation to work with tags

--- application/controllers/TagController.php
+++ application/controllers/TagController.php
@@ -12,4 +12,23 @@ class TagController extends Hermes_Controller_SessionController{
 		$tagModel = new Application_Model_TagModel($this->curUser);
 		$tagModel->addTag($tagName,false);
 	}
+	public function getDisplayAction(){
+		 $this->_helper->viewRenderer->setNoRender();
+		 $this->_helper->layout()->disableLayout();
+		 $tagModel = new Application_Model_TagModel();
+		 $tagModel->setRankedTags(2);
+		 $rankedTags = $tagModel->getRankedTags();
+		 $jsonArray = array();
+		 if($rankedTags){
+			 foreach($rankedTags as $tag){
+			 	$subArray['value']=$tag->getTagName();
+			 	$subArray['name']=$tag->getTagName();
+			 	$jsonArray[] = $subArray; 
+			 }
+		 }else{
+		 	$jsonArray = "";
+		 }
+		 echo json_encode($jsonArray);
+		 //returns a JSON delimited series of tags to show
+	}
 }
