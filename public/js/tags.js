b895f27e75fa106bfb5c3213615949b8de171b7d
Andy Tsen <your_email@youremail.com>
2011-10-09 12:00:21 -0700

    styling to show and hide tag divs

--- public/js/tags.js
+++ public/js/tags.js
@@ -2,6 +2,7 @@
  * 
  */
 var Tags = {
+	showTagInput:false,
 	init:function(){
 		var data;
 		$.get('/tag/get-display',
@@ -9,12 +10,20 @@ var Tags = {
 				  var stats = null;
 				  eval('stats='+results);
 				  $(".tag_input").autoSuggest(stats);
-			  
 		});
-		
 	},
 	fetchTagData:function(){
 		
+	},
+	toggleDropDown:function(){
+		if(!Tags.showTagInput){
+			Tags.showTagInput = true;
+			jQuery(".tag_input_wrapper").slideDown('slow');
+		}
+		else if(Tags.showTagInput){
+			Tags.showTagInput = false;
+			jQuery(".tag_input_wrapper").slideUp('slow');
+		}
 	}
 		
 }
