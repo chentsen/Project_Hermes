4a9d6434819d433e79e5aa0ef9d635ac96b92207
Andy Tsen <your_email@youremail.com>
2011-10-13 01:12:15 -0700

    adding production push script

---
+++ production-push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Pushing Production branch to PRODUCTION"
+ssh ec2-user@ec2-50-17-154-145.compute-1.amazonaws.com "cd /PT/production/Project_Hermes;sudo git reset --hard HEAD;sudo git checkout production;sudo git pull origin production;35nxcab$;exit;"
