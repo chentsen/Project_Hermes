1a6f4c26a8f2e13b90e5dc79557037e52274d607
Andy Tsen <your_email@youremail.com>
2011-10-15 16:28:54 -0700

    Changing application to production!

--- production-push.sh
+++ production-push.sh
@@ -2,5 +2,5 @@
 
 echo "Pushing Production branch to PRODUCTION"
 
-ssh ec2-user@ec2-184-73-13-197.compute-1.amazonaws.com "cd /PT/production/Project_Hermes;sudo git reset --hard HEAD;sudo git checkout production;sudo git pull origin production;echo 'DONE'"
+ssh ec2-user@ec2-184-73-13-197.compute-1.amazonaws.com "cd /PT/production/Project_Hermes;sudo git reset --hard HEAD;sudo git checkout production;sudo git pull origin production;echo 'DONE';"
 
