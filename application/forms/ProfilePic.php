e27203ca427199e1dc10251230fc018490e4d037
Andy Tsen <your_email@youremail.com>
2011-10-16 19:45:29 -0700

    limiting file size images

--- application/forms/ProfilePic.php
+++ application/forms/ProfilePic.php
@@ -11,7 +11,7 @@ class Application_Form_ProfilePic extends Zend_Form{
 		$image = new Zend_Form_Element_File('image');
 		$image->removeDecorator('label')
 		      ->setRequired(true)
-		      ->setMaxFileSize(10240000); // limits the filesize on the client side
+		      ->setMaxFileSize(2097152); // limits the filesize on the client side
 		      //->setDescription('Click Browse and click on the image file you would like to upload');
 		$image->addValidator('Count', false, 1);                // ensure only 1 file
 		$image->addValidator('Size', false, 2097152);            // limit to 10 meg
