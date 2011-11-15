bbd44efbbd6c8421e64782abfdd98224eb64161f
Julian Tai <juliantai90@gmail.com>
2011-11-15 02:15:33 -0800

    modal styles

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -15,17 +15,29 @@ class Application_Form_Login extends Zend_Form
 		$this->setMethod('post');
 		$email = new Zend_Form_Element_Text('email');
 		$email->setRequired(true)
-			  ->removeDecorator('label')
-			  ->removeDecorator('htmlTag')
-			  ->addValidator("EmailAddress",true,array('messages'=>
-			  											array('emailAddressInvalidFormat' => 
-			  												  'Please enter a valid email address. EX: jane@emailaddress.com')));
-			  										
-		
+			  ->addValidator('EmailAddress',true, array(
+     			'messages' => array(
+          			'emailAddressInvalidFormat' => 'Please enter a valid email address. EX: jane@emailaddress.com'
+    			 )))
+			  ->addValidator('NotEmpty', true, array(
+     			'messages' => array(
+          			'isEmpty' => 'An email address is required.'
+    			 )
+  				));
+			  
 		$password = new Zend_Form_Element_Password('password');
-		$password->setRequired(true)
+		$password->addValidator('Regex',false,array('pattern' => '/^.*(?=.{6,20})(?=.*[\d])(?=.*[a-zA-Z])/',
+													'messages'=>array('regexNotMatch' => "Passwords must contain 6 to 20 characters and have at least one number")))
+				 ->addValidator('StringLength',false,array('max'=>20))
+				 ->setRequired(true)
+				 ->addValidator('NotEmpty', true, array(
+     					'messages' => array(
+          				'isEmpty' => 'A password is required.'
+    					 )
+  				  ))
 				 ->removeDecorator('label')
 				 ->removeDecorator('htmlTag');
+		
     	
 		$login = new Zend_Form_Element_Submit('login');
 		$login->removeDecorator('label')
