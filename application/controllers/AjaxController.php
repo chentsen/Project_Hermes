6773c2b2035769cf9a5290627588cf831f0ab579
Julian Tai <juliantai90@gmail.com>
2011-11-28 23:55:47 -0800

    ajax registration fixed

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -13,13 +13,26 @@ class AjaxController extends Zend_Controller_Action{
 	 }
     public function ajaxformAction()
 	{
+		 $this->_helper->viewRenderer->setNoRender();
+                  $this->_helper->getHelper('layout')->disableLayout();
+				  $form = new Application_Form_Registration();
+				   $form->isValid($this->_getAllParams());
+				  $json = $form->getMessages();
+				  header('Content-type: application/json');
+				  echo Zend_Json::encode($json);
+		
+	}
+	public function ajaxloginAction()
+	{
+				  
 		$this->_helper->viewRenderer->setNoRender();
                 $this->_helper->getHelper('layout')->disableLayout();
                 
                 //pull content json
-                $form = new Application_Form_Registration();
+                $form = new Application_Form_Login();
                 $form->isValid($this->_getAllParams());
-		$json = $form->getMessages();
+				  $json = $form->getMessages();
+				 
 		header('Content-type: application/json');
 		echo Zend_Json::encode($json);
 	}
