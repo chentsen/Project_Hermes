836f333d250e53eb9e16a9653f511752d949741e
Yoshi Miyake <y.miyake@gmail.com>
2012-01-18 13:29:24 -0800

    go button

--- application/views/helpers/DisplayFeed.php
+++ application/views/helpers/DisplayFeed.php
@@ -17,7 +17,7 @@ class Application_View_Helper_DisplayFeed extends Zend_View_Helper_Abstract{
 		}
 	public function getEventFeedMessage(FeedObject $feedObject){
 		$creatorEmail = $feedObject->getCreator()->getEmail();
-		echo "<img style='float: left; margin-right: 5px; margin-top: 2px;' src='/images/calendar.png' width='30' height='30'/>";
+		echo "<img style='float: left; margin-right: 8px; margin-top: 1px;' src='/images/calendar.png' width='30' height='30'/>";
 		echo "<div class='individual-object'>Your friend <a href='/profile/public/email/{$creatorEmail}'> ".addslashes($feedObject->getCreator()->getFirstName())." ".addslashes($feedObject->getCreator()->getLastName())."</a> wants to";
 		echo " <a href = '/event/index/eid/". $feedObject->getEid()."'>".$feedObject->getShortDescription()."</a>.";
 		//echo '<br /> at '.$feedObject->getDate()->format('m/d');
@@ -26,7 +26,7 @@ class Application_View_Helper_DisplayFeed extends Zend_View_Helper_Abstract{
 	
 	public function getFriendAcceptFeedMessage(FeedObject $feedObject){
 		$creatorEmail = $feedObject->getEmail();
-		echo "<img style='float: left; margin-right: 5px; margin-top: 2px;' src='/images/placeholder.png' width='30' height='30'/>";
+		echo "<img style='float: left; margin-right: 8px; margin-top: 1px;' src='/images/placeholder.png' width='30' height='30'/>";
 		echo "<div class='individual-object'>You and <a href='/profile/public/email/{$creatorEmail}'> ".addslashes($feedObject->getFirstName())." ".addslashes($feedObject->getLastName())."</a> became friends.";
 		echo '<br /> <span>'.$feedObject->getDate()->format('F jS, Y h:i A').'</span></div>';
 	}
