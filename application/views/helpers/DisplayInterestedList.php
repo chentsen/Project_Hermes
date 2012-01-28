453b47c1f613290233ad23f6016470f053c1994f
Yoshi Miyake <y.miyake@gmail.com>
2012-01-27 18:13:49 -0800

    asdsadas

--- application/views/helpers/DisplayInterestedList.php
+++ application/views/helpers/DisplayInterestedList.php
@@ -12,7 +12,7 @@ class Zend_View_Helper_DisplayInterestedList extends Zend_View_Helper_Abstract{
 		echo '<div class = "waitingList">';
 		foreach($waitingList as $user){
                         echo '<div class="waitingImg"><img height="100" width="100 "src="/images/placeholder.png" /></div>';
-			echo "<div class="waiting-details">{$user->getFirstName()} wants to come.</div>";
+			echo "<div class='waiting-details'>{$user->getFirstName()} wants to come.</div>";
 			echo "<div class='event_allow'><a href = '/event/response/response/y/eid/{$event->getEid()}/email/{$user->getEmail()}'>Accept</a> | <a href = '/event/response/reponse/y/eid/{$event->getEid()}/email/{$user->getEmail()}'>Ignore</a></div>";
 		}
 		echo '</div>';
