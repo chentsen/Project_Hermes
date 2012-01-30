86a58a79f8de2bc0f7dae36805ce20c00d03be63
Andy Tsen <your_email@youremail.com>
2012-01-29 19:15:09 -0800

    adding fb logout

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+var Login = {
+    logout:function(){
+        $.ajax({
+            url:'/index/logout/',
+            data:{},
+            success:function(data){
+                //just try logging out twice for now, change to once later
+                FB.logout(function(){
+                    window.location=data.redirect;
+                })
+                
+            }
+        });
+        
+    }
+    
+}
