5963cf25af35a54a430ffe5b6fcf55bdf82e20cd
Andy Tsen <your_email@youremail.com>
2012-01-31 00:57:45 -0800

    fixing js

--- public/js/fb_interface.js
+++ public/js/fb_interface.js
@@ -1,18 +1,4 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '276813252365802',
-        status     : true, 
-        cookie     : true,
-        xfbml      : true,
-        oauth      : true
-    });
-};
-(function(d){
-    var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-    js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    d.getElementsByTagName('head')[0].appendChild(js);
-}(document));
+
 /*Bridge between Facebook SDK and PT */
 var FBI = {
     authenticate:function(){
