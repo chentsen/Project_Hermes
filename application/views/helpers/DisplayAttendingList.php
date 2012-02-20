b9ef3782b0a8637f837aff30c0681cc2bb242108
Julian Tai <juliantai90@gmail.com>
2012-02-19 20:45:17 -0800

    list of people going

---
+++ application/views/helpers/DisplayAttendingList.php
@@ -0,0 +1,22 @@
+<?php
+
+class Zend_View_Helper_DisplayAttendingList extends Zend_View_Helper_Abstract{
+	
+	public function DisplayAttendingList($event){
+		$attendingList = $event->getMembers();
+		echo '<div class = "attendingList">';
+		foreach($attendingList as $user){
+			$eid = $event->getEid();
+			$uid = $user->getUid();
+			echo '<div class="attending-item attending-item-'.$uid.'">';
+			echo "<a href='/profile/public/uid/{$user->getUid()}'>";
+                        echo "<img src='/img/profile-pic/uid/{$user->getUid()}' height=100  width=100/></a>";
+			echo "<div class='attending-details'>{$user->getFirstName()} is attending.</div>";
+			//echo "<div class='event_allow'><a onclick=\"Event.processJoinRequest('{$eid}',{$uid},'y')\">Accept</a> | <a onclick = \"Event.processJoinRequest('{$eid}',{$uid},'n')\">Ignore</a></div>";
+			echo '</div>';
+		}
+		echo '</div>';
+		
+	}
+	
+}
