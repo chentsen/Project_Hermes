0d382e5cd490fae98782c260313cc62df5ead886
Yoshi Miyake <y.miyake@gmail.com>
2012-02-22 00:55:36 -0800

    friend requests

--- application/views/helpers/DisplayPendingRequests.php
+++ application/views/helpers/DisplayPendingRequests.php
@@ -6,18 +6,17 @@ class Zend_View_Helper_DisplayPendingRequests extends Zend_View_Helper_Abstract{
 		//can possibly pass this object from calling controller, view..
 		$this->friendRelation = new Application_Model_FriendRelation($identity);
 		$friendRequests = $this->friendRelation->getRequestList();
-		echo "<li class=friend-request>";
+
 		//echo 'found friernd';
 		if($friendRequests){
 			//print_r($friendRequests);
 			foreach($friendRequests as $friendRequest){
 				$lastName = substr($friendRequest->getRequester()->getLastName(),0,1);
-				echo "<div class='friend-request-message'><a class='friend-request-name' href='/profile/public/uid/{$friendRequest->getRequester()->getUid()}'>{$friendRequest->getRequester()->getFirstName()} {$lastName}.</a> would like to be your friend.</div>";
+				echo "<li class='friend-requests'><div class='friend-request-message'><a class='friend-request-name' href='/profile/public/uid/{$friendRequest->getRequester()->getUid()}'>{$friendRequest->getRequester()->getFirstName()} {$lastName}.</a> would like to be your friend.</div>";
 				echo "<div class='accept-ignore'><a class='friend-request-accept' href = '/friend/respond_friend_request/accept/yes/rid/{$friendRequest->getRequestId()}'> Accept </a>
-					  <a class='friend-request-ignore' href = '/friend/respond_friend_request/accept/no/rid/{$friendRequest->getRequestId()}'> Ignore </a></div>";
+					  <a class='friend-request-ignore' href = '/friend/respond_friend_request/accept/no/rid/{$friendRequest->getRequestId()}'> Ignore </a></div></li>";
 			}
 		}
 		else echo 'You have no pending friend requests.';
-		echo "</li>";
 	}
 }
